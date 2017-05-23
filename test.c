2795e1a6b599f988a967b0d3b7fcf1c8ec77b188
daydream725 <daydream725@naver.com>
2017-05-23 12:10:34 +0900

    no message

--- test.c
+++ test.c
@@ -1,5 +1,8 @@
 
 #include <stdio.h>
 int main() {
-	printf("test.c");
-	\}
+printf("test");
+}[A[D	[D[D[D[D[D[D[D[D[D[D  [2~	[D[D[D[D[D[D[D[D[D[D[C[C i[D[3~[3~[3~[3~[3~[3~[3~[3~printf("test");
+
+
+[A[A[C
