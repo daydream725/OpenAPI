998997475f18178060904a14e0bfb06a79045b7d
daydream725 <daydream725@naver.com>
2017-05-23 11:41:42 +0900

    test.c change

--- test.c
+++ test.c
@@ -1,8 +1,5 @@
 
 #include <stdio.h>
 int main() {
-printf("test");
-}[A[D	[D[D[D[D[D[D[D[D[D[D  [2~	[D[D[D[D[D[D[D[D[D[D[C[C i[D[3~[3~[3~[3~[3~[3~[3~[3~printf("test");
-
-
-[A[A[C
+	printf("test.c");
+	\}
